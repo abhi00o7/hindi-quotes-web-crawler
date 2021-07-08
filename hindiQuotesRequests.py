a54fcd569ffc889a4f294477bdf01054ba911013
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-07-08 23:36:04 +0530

    Update hindiQuotesRequests.py

--- hindiQuotesRequests.py
+++ hindiQuotesRequests.py
@@ -5,9 +5,16 @@ from bs4 import BeautifulSoup
 
 # hindi quotes website आज का सुविचार
 r = requests.get('https://aajkasuvichar.com/aaj-ka-suvichar/')
-content = urllib.request.urlopen()
+# content = urllib.request.urlopen('https://aajkasuvichar.com/aaj-ka-suvichar/')
+# content = urllib.request.urlopen('https://www.hindishayari.biz/2019/11/motivation-shayari-in-hindi-suvichar-in.html')
+content = urllib.request.urlopen('https://api.github.com/events')
 # r = requests.get('https://api.github.com/events')
 
-# print(r.status_code)
-print(r.text)
+read_content = content.read()
+
+soup = BeautifulSoup(read_content, 'html.parser')
+print(r.status_code)
+# print(r.text)
+# print(read_content)
+# print(soup)
 
