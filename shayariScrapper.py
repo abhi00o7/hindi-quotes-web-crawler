16f806f5d499e6c2edca31ac96a7ec868f78a073
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-07-09 14:38:02 +0530

    added exception handling and main block

--- shayariScrapper.py
+++ shayariScrapper.py
@@ -1,38 +1,59 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
-# crawl to page with requests
-# url = "'https://www.digitalalia.in/2020/01/motivational-shayari-hindi.html"
-url = "https://www.digitalalia.in/2019/10/suvichar-hindi.html"
-r = requests.get(url)
 
-# parse the output with html parser
-soup = BeautifulSoup(r.text, 'html.parser')
 
-#filter all the required attributes
-quotes = soup.find_all('blockquote', attrs={'class': 'tr_bq'})
+def shayariScrapper():
+    # crawl to page with requests
+    # url = "'https://www.digitalalia.in/2020/01/motivational-shayari-hindi.html"
+    url = "https://www.digitalalia.in/2019/10/suvichar-hindi.html"
 
-#get the length
-print('Total Number of Cards Found:', len(quotes) )
+    try:
+        r = requests.get(url)
 
-count = 1
+        # parse the output with html parser
+        soup = BeautifulSoup(r.text, 'html.parser')
 
-#array to store the quotes
-hindiQuotes = []
+        # filter all the required attributes
+        # need to be changed for diffrent websites a/c page structure.
+        quotes = soup.find_all('blockquote', attrs={'class': 'tr_bq'})
 
-#debug for the length and print the output
-for post in quotes:
-    #initializing a dictionary
-    quotes_data = {}
+        # get the length
+        print('Total Number of Cards Found:', len(quotes))
 
-    # creating a data dictionary
-    quotes_data['quoteText'] = post.text.replace('\n', '')
-    quotes_data['quoteAuthor'] = "आज का सुविचार"
-    hindiQuotes.append(quotes_data)
+        count = 1
 
-# create a data frame for the list of dictionaries 
-dataFrame = pd.DataFrame(hindiQuotes)
+        # array to store the quotes
+        hindiQuotes = []
 
-print(dataFrame)
-# save to json file
-dataFrame.to_json("hindiMotivationalQuotes.json", orient="records", force_ascii = False)
+        # debug for the length and print the output
+        for post in quotes:
+            # initializing a dictionary
+            quotes_data = {}
+
+            # creating a data dictionary
+            quotes_data['quoteText'] = post.text.replace('\n', '')
+            quotes_data['quoteAuthor'] = "आज का सुविचार"
+            hindiQuotes.append(quotes_data)
+
+        # create a data frame for the list of dictionaries
+        dataFrame = pd.DataFrame(hindiQuotes)
+
+        print(dataFrame)
+        # save to json file
+        # dataFrame.to_json("hindiMotivationalQuotes.json", orient="records", force_ascii = False)
+
+    except SyntaxError:
+        print("Syntax not defined properly!")
+
+    except NameError:
+        print("some variable(s) is(are) not defined.")
+
+    except:
+        print("An exception occurred!")
+
+# main
+
+
+if __name__ == "__main__":
+    shayariScrapper()
