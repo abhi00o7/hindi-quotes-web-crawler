a59669c9d7abb6c284bedcbed24670e94eaf263b
Abhishek Kumar <abhishek.ak.vii@gmail.com>
2021-07-09 14:28:51 +0530

    updated json && added exception handling

--- hindiShayariScrapper.py
+++ hindiShayariScrapper.py
@@ -3,37 +3,46 @@ import pandas as pd
 from bs4 import BeautifulSoup
 # crawl to page with requests
 url = "https://hindishayarimala.in/best-suvichar-shayari-and-quotes-in-hindi/"
-r = requests.get(url)
+# url = "" #placeholder to check your array
 
-# parse the output with html parser
-soup = BeautifulSoup(r.text, 'html.parser')
+try:
+    r = requests.get(url)
 
-#filter all the required attributes
-quotes = soup.find_all('blockquote', attrs={
-                       'class': 'has-text-color has-very-dark-gray-color'})
+    # parse the output with html parser
+    soup = BeautifulSoup(r.text, 'html.parser')
 
-#get the length
-print('Total Number of Cards Found:', len(quotes))
+    #filter all the required attributes
+    quotes = soup.find_all('blockquote', attrs={'class': 'has-text-color has-very-dark-gray-color'})
 
-count = 1
+    #get the length
+    print('Total Number of Cards Found:', len(quotes))
 
-#array to store the quotes
-hindiQuotes = []
+    #array to store the quotes
+    hindiQuotes = []
 
-#debug for the length and print the output
-for post in quotes:
-    #initializing a dictionary
-    quotes_data = {}
+    #debug for the length and print the output
+    for post in quotes:
+        #initializing a dictionary
+        quotes_data = {}
 
-    # creating a data dictionary
-    quotes_data['quoteText'] = post.text.replace('\n', '')
-    quotes_data['quoteAuthor'] = "आज का सुविचार"
-    hindiQuotes.append(quotes_data)
+        # creating a data dictionary
+        quotes_data['quote'] = post.text.replace('\n', '')
+        quotes_data['Author'] = "आज का सुविचार"
+        hindiQuotes.append(quotes_data)
 
-# create a data frame for the list of dictionaries
-dataFrame = pd.DataFrame(hindiQuotes)
+    # create a data frame for the list of dictionaries
+    dataFrame = pd.DataFrame(hindiQuotes)
 
-print(dataFrame)
-# save to json file
-dataFrame.to_json("hindiMotivationalQuotes.json",
-                  orient="records", force_ascii=False)
+    print(dataFrame)
+
+    # save to json file
+    # dataFrame.to_json("hindiMotivationalQuotes.json", orient = "records", force_ascii = False)
+
+except SyntaxError:
+    print("Syntax not defined properly!")
+
+except NameError:
+    print("some variable(s) is(are) not defined.")
+
+except :
+    print("An exception occurred!")
